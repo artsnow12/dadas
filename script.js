3b7c8cdeeec4252d9089de0d8a2ef15a6ce0228c
artsnow12 <168270648+artsnow12@users.noreply.github.com>
2025-04-27 18:42:49 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function promptNumbers() {
+    for (let i = 0; i < 10; i++) {
+        const userInput = prompt(`Введіть число більше 100`);
+
+        if (userInput === null || userInput === "" || isNaN(userInput)) {
+            console.log(" спробуйте ввести ще раз");
+            continue;
+        }
+
+        const number = Number(userInput);
+        if (number > 100) {
+            console.log("Ви ввели число більше 100");
+            break;
+        } else {
+            console.log("Ви ввели число менше або рівне 100, введіть число більше 100");
+        }
+    }
+}
+
+promptNumbers();
